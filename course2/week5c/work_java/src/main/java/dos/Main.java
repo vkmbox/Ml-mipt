749d0b87fec1b8409a8e08d91c381dba266b2b51
vkmbox <vkmbox@yandex.ru>
2018-07-21 21:35:29 +0300

    by 21/07

--- course2/week5c/work_java/src/main/java/dos/Main.java
+++ course2/week5c/work_java/src/main/java/dos/Main.java
@@ -1,32 +1,26 @@
 package dos;
 
-//import org.apache.commons.math4.linear.Array2DRowRealMatrix;
-//import org.apache.commons.math4.linear.RealMatrix;
-//import org.apache.commons.math4.linear.RealVector;
+import static java.util.stream.Collectors.toList;
+
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.linear.MatrixUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 public class Main{
   
-  public static double distance_cosine(RealVector aa, RealVector bb) {
+  private double distance_cosine(RealVector aa, RealVector bb) {
     return 1 - aa.cosine(bb);
   }
   
-  public static void main(String ... args) throws IOException {
-    List<RealVector> xtrain = new ArrayList<>();
-    try (InputStream xStream = Main.class.getResourceAsStream("xtrain.csv");
+  private List<RealVector> xList(String resource) throws IOException {
+    List<RealVector> result = new ArrayList<>();
+    try (InputStream xStream = Main.class.getResourceAsStream(resource);
       Scanner scanner = new Scanner(xStream).useDelimiter(";")) {
       int count = 0;
       double[] buffer = null;
@@ -36,15 +30,81 @@ public class Main{
         }
         buffer[count++] = Double.parseDouble(scanner.next());
         if ( count == 64 ) {
-          xtrain.add(MatrixUtils.createRealVector(buffer));
+          result.add(MatrixUtils.createRealVector(buffer));
+          count = 0;
         }
       }
-      //String result = scanner.hasNext() ? scanner.next() : "";
+      assert count == 0 : "brain drain";
     }
+    return result;
+  }
+
+  private List<Integer> yList(String resource) throws IOException {
+    List<Integer> result = new ArrayList<>();
+    try (InputStream xStream = Main.class.getResourceAsStream(resource);
+      Scanner scanner = new Scanner(xStream).useDelimiter(";")) {
+      while (scanner.hasNext()) {
+        result.add(Integer.parseInt(scanner.next()));
+      }
+    }
+    return result;
+  }
+  
+  public void oneNearestNeighbors() throws IOException {
+
+    List<RealVector> data = xList("data.csv");
+    List<Integer> target = yList("target.csv");
+
+    int trainSize = (int) Math.round(target.size() * 0.25) ;
+    
+    final List<RealVector> xtest = data.subList(0, trainSize - 1);
+    final List<RealVector> xtrain = data.subList(trainSize, target.size());
+    final List<Integer> ytest = target.subList(0, trainSize - 1);
+    final List<Integer> ytrain = target.subList(trainSize, target.size());
+    
+    /*int trainSize = (int) Math.round(target.size() * 0.75) ;
+    
+    final List<RealVector> xtrain = data.subList(0, trainSize - 1);0,040089
+    final List<RealVector> xtest = data.subList(trainSize, target.size());
+    final List<Integer> ytrain = target.subList(0, trainSize - 1);
+    final List<Integer> ytest = target.subList(trainSize, target.size());*/
+    
+    assert xtrain.size() == ytrain.size() : "Invalid train data";
+    assert xtest.size() == ytest.size() : "Invalid test data";
+    
+    Function< RealVector, Integer > estimator = ( RealVector arg ) -> {
+      int pos = -1;
+      double distance = 10.0;
+      for ( int ii = 0; ii < xtrain.size(); ii++ ) {
+        double dist = distance_cosine(arg, xtrain.get(ii));
+        if ( distance > dist ) {
+          distance = dist;
+          pos = ii;
+        }
+      }
+      assert pos > -1 : "Posution not found";
+      return ytrain.get(pos);
+    };
+    
+    List<Integer> yEstimation = xtest.stream().map(estimator).collect(toList());
+    int count = 0;
+    for ( int ii = 0; ii < ytest.size(); ii++ ) {
+      if ( ytest.get(ii).equals( yEstimation.get(ii) ) )
+        count++;
+    }
+    double accuracy = (double)count/(double)ytest.size();
+    System.out.println(String.format("AntiAccuracy is : %f", 1-accuracy));
+  }
+  
+  public static void main(String ... args) throws IOException {
+    new Main().oneNearestNeighbors();
+  }
+      //RealVector debug = xtrain.get(xtrain.size()-1); //[xtrain.length-1];
+      //System.out.println(xtrain.size());
+      //String result = scanner.hasNext() ? scanner.next() : "";
       //InputStreamReader reader = new InputStreamReader(xStream, "UTF-8"); ) {
       //IOUtils.copy(inputStream, writer, encoding);
 //String theString = writer.toString();
-    }
     //File file = new File("E:\\MyProj\\Python\\curs\\1\\Task1a\\cats.txt");
     /*String file = "E:\\MyProj\\Python\\curs\\1\\Task1a\\cats.txt";
     List<List<String>> lines = 
