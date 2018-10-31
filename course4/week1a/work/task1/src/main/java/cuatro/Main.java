f8febcf7e7b959ecc95b5f3ca3857ecac4b8925a
aa <aa@bb>
2018-10-31 23:38:14 +0300

    by 31/10

--- course4/week1a/work/task1/src/main/java/cuatro/Main.java
+++ course4/week1a/work/task1/src/main/java/cuatro/Main.java
@@ -5,10 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.util.FastMath;
 
 public class Main {
   
@@ -28,25 +30,40 @@ public class Main {
     }
   }
   
-  public static void main(String args) throws IOException {
+  public static double[] confidenceInterval( double[] values, double alpha ) {
+    if (values.length == 0 ) return null;
+    NormalDistribution normal = new NormalDistribution();
+    double quantile = normal.inverseCumulativeProbability(1 - alpha/2);
+    //double std = FastMath.sqrt(StatUtils.variance(values));
+    double high = StatUtils.mean(values) 
+        + quantile * FastMath.sqrt(StatUtils.variance(values)/values.length);
+    double low = StatUtils.mean(values) 
+        - quantile * FastMath.sqrt(StatUtils.variance(values)/values.length);
+    return new double[] {low, high};
+  }
+  
+  public static void main(String ... args) throws IOException {
     String file = "c:\\MyProj\\Coursera\\Ml-mipt\\course4\\week1a\\work\\task1\\water.txt";
-    List<Row> rows = Files.lines(Paths.get(file)).map(Row::fromString).collect(Collectors.toList());
+    List<Row> rows = Files.lines(Paths.get(file)).skip(1).map(Row::fromString).collect(Collectors.toList());
     /*
-    Постройте 95% доверительный интервал для средней годовой смертности в больших городах. 
-    Чему равна его нижняя граница? Округлите ответ до 4 знаков после десятичной точки.    
-    */
+    Постройте 95% доверительный интервал для средней годовой смертности в больших городах.
+    Чему равна его нижняя граница? Округлите ответ до 4 знаков после десятичной точки.
+     */
+    double[] mort1 = rows.stream().mapToDouble(Row::getMortality).toArray();
+    double[] interval1 = confidenceInterval(mort1, 0.05);
+    System.out.println(interval1[0]);
     /*
-    На данных из предыдущего вопроса постройте 95% доверительный интервал для средней 
-    годовой смертности по всем южным городам. Чему равна его верхняя граница? 
-    Округлите ответ до 4 знаков после десятичной точки.    
-    */
+    На данных из предыдущего вопроса постройте 95% доверительный интервал для средней
+    годовой смертности по всем южным городам. Чему равна его верхняя граница?
+    Округлите ответ до 4 знаков после десятичной точки.
+     */
     /*
-    На тех же данных постройте 95% доверительный интервал для средней годовой 
+    На тех же данных постройте 95% доверительный интервал для средней годовой
     смертности по всем северным городам. Пересекается ли этот интервал с предыдущим? 
     Как вы думаете, какой из этого можно сделать вывод?
-    */
+     */
     /*
-    Пересекаются ли 95% доверительные интервалы для средней жёсткости воды в северных и южных городах?    
-    */
+    Пересекаются ли 95% доверительные интервалы для средней жёсткости воды в северных и южных городах?
+     */
   }
 }
